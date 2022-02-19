4e852f29ddc7f1c7712f4f4b943181186bd577a9
Jonathan Velez <javc2006@hotmail.com>
2022-02-19 11:43:56 -0500

    table users created

---
+++ backend/users/database/migrations/2022_02_19_160215_create_users_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name', 100);
+            $table->string('last_name', 100);       
+            $table->integer('dni')->unique();
+            $table->string('email', 150)->unique();     
+            $table->string('country');     
+            $table->string('street_address', 180);     
+            $table->integer('cell_phone');     
+            $table->integer('category_id')->unsigned();     
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+        });
+
+        Schema::table('users', function($table) {
+           $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
