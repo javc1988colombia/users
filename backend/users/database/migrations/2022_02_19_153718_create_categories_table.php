debd166b96493ac915b04973ce36e36b51bb705a
Jonathan Velez <javc2006@hotmail.com>
2022-02-19 10:58:37 -0500

    create table categories

---
+++ backend/users/database/migrations/2022_02_19_153718_create_categories_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('description', 255);            
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+        });
+
+        DB::table('categories')->insert(
+           [
+            ['description' => 'Cliente'],    
+            ['description' => 'Proveedor'],
+            ['description' => 'Funcionario Interno'] 
+           ]
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
