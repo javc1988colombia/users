de9d81745ea7008644fe255ff72464099c6470f8
Jonathan Velez <javc2006@hotmail.com>
2022-02-20 12:12:49 -0500

    added front

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,27 +1,10 @@
-import {Component, ElementRef, ViewChild} from '@angular/core';
-import {ClientOrdersService} from "./client-orders.service";
-import {ClientOrders} from "./client_orders";
-import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
-  title = 'test';
-
-  clientOrders: ClientOrders[] = [];
-
-  constructor(private clientOrdersService: ClientOrdersService,) {}
-
-  ngOnInit(): void {
-    this.getClientOrders();
-  }
-
-  getClientOrders(): void {
-    this.clientOrdersService.getClientOrders()
-      .subscribe(heroes => this.clientOrders = heroes);
-  }
+  title = 'frontend';
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,22 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HttpClientModule } from '@angular/common/http';
-import { OrderFormComponent } from './order-form/order-form.component';
-import { OrderDetailsComponent } from './order-details/order-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    OrderFormComponent,
-    OrderDetailsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    NgbModule,
-    HttpClientModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
